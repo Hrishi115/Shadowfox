de3d9f1e0bdc851b4cbfbd650f951241cf327b55
Hrishi115 <hrishikeshshendage313@gmail.com>
2025-07-07 18:45:04 +0530

    Numbers task done

--- Task1(Beginner)/Numbers.py
+++ Task1(Beginner)/Numbers.py
@@ -1,3 +1,4 @@
+#Subtask 1
 def formatfunc(num, srt):
     return format(num, srt)
 
@@ -9,3 +10,17 @@ print(result)
 # num, takes the number input which needs to be formatted and srt, which takes input about which formatting to be used.
 # For example, 'o' is octal conversion, 'b' is binary coversion, 'x' for hexadecimal coversion.
 # The format() function here, converts 145 into octal format which becomes 221.
+
+#subtask 2
+pi = 3.14
+radius = 84 
+area = pi*radius*radius
+# print(f"The Area of the pond: {area}")
+water_per_square_meter = 1.4
+Total_water_in_pond = int(water_per_square_meter*area)
+print(f"The total water present in the pond: {Total_water_in_pond} Litres")
+
+#subtask 3
+Distance = 490 #in meters
+time = 7*60
+print(f"Speed: {int(Distance/time)} m/s")
