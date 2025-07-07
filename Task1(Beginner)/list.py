2ce603e3177c90a42a37f91c5d83c1b127bec5e5
Hrishi115 <hrishikeshshendage313@gmail.com>
2025-07-07 20:00:00 +0530

    List Task Done

---
+++ Task1(Beginner)/list.py
@@ -0,0 +1,17 @@
+justice_league = ["Superman","Batman","Wonder Woman","Flash","Aquaman","Green Lantern"]
+length = len(justice_league)
+justice_league.extend(["Batgirl", "Nightwing"])
+# print(justice_league)
+
+justice_league.remove("Wonder Woman")
+justice_league.insert(0, "Wonder Woman")
+# print(justice_league)
+
+justice_league.remove("Superman")
+pos = justice_league.index("Aquaman")
+justice_league.insert(pos + 1, "Superman")
+print(justice_league)
+
+justice_league = ["Cyborg", "Shazam", "Hawkgirl", "Martian Manhunter", "Green Arrow"]
+justice_league.sort()
+# print(justice_league)
