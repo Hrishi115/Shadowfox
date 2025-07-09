b47634beb3e22fce354e45f3f8cc91d4165bd322
Hrishi115 <hrishikeshshendage313@gmail.com>
2025-07-09 22:24:26 +0530

    For Loop2 corrected

--- Task1(Beginner)/For_Loop2.py
+++ Task1(Beginner)/For_Loop2.py
@@ -3,15 +3,22 @@ import time
 for i in range (1,11):
     print("Perfrom 10 jumping jacks")
     time.sleep(20)
+    
+    completed = i*10
+    
+    if completed == 100:
+        print("Congratulations! You completed all 100 Jumping Jacks!")
+
     user_in = input("Are you tired?(reply 'yes' or 'y'): ").lower().strip()
+
     if user_in == "yes" or user_in == "y":
-        user_in = input("Do you want to skip the remaining sets?: ").lower().strip()
-        if user_in == "yes" or user_in == "y":
-            print(f"You Completed a total of {10*i} jumping jacks!")
+        skip = input("Do you want to skip the remaining sets? (yes/y or no/n): ").lower().strip()
+        if skip == "yes" or user_in == "y":
+            print(f"You Completed a total of {completed} jumping jacks!")
             break
         else: 
-            continue
-    else:
-        continue
-else:
-    print("Congratulations! You completed all 100 Jumping Jacks!")
+            remaining = 100 - completed
+            print(f"{remaining} jumping jacks remaining!")
+    elif user_in == "no" or user_in == "n":
+        remaining = 100 - completed
+        print(f"You Completed a total of {completed} jumping jacks!")
